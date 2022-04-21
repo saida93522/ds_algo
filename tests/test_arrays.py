08afeaeeca0859ff61eef7f1c8391d5b64f34010
saida93522 <54591143+saida93522@users.noreply.github.com>
2022-04-21 11:15:06 -0500

    test: test array rotates

--- tests/test_arrays.py
+++ tests/test_arrays.py
@@ -2,6 +2,7 @@ import unittest
 
 from Data_Structures.Array import contains_duplicate as duplicate
 from Data_Structures.Array import merge_sorted_array as merged_array
+from Data_Structures.Array import rotate_array as rotate_array
 
 
 class TestArrays(unittest.TestCase):
@@ -22,3 +23,15 @@ class TestArrays(unittest.TestCase):
         expected = merged_array.merge_arrays(arr1,arr2)
         actual = [1,2,3,4,5,6,7,8,9,10]
         self.assertEqual(expected,actual)
+
+    def test_array_rotated_right(self):
+        arr = [10,11,12,13,14,15,16,17]
+        n = 4
+        expected = rotate_array.rotate_right(arr,n)
+        self.assertEqual(expected, [14,15,16,17,10,11,12,13])
+
+    def test_array_rotated_left(self):
+        arr = [14,15,16,17,10,11,12,13]
+        n = 4
+        expected = rotate_array.rotate_left(arr,n)
+        self.assertEqual(expected, [10,11,12,13,14,15,16,17])
